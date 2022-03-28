f67121964b49e6109a8e1365da21cb8393c805a8
Sneha Gupta <71871134+sneha281@users.noreply.github.com>
2022-03-28 13:49:48 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,28 @@
+// Questions as an array of Objects
+var questions = [
+    {
+      title: 'Which of the following is used to create Web Pages?',
+      choices: ['HTML', 'C#', 'Python', 'C++'],
+      answer: 'HTML'
+    },
+    {
+      title: 'HTML stands for',
+      choices: ['none of the above', 'Hyper Tech Markup Language', 'Hyper Text Markup Language', 'Hyper Text Makeup Language'],
+      answer: 'Hyper Text Markup Language'
+    },
+    {
+      title: 'HTML is considered as',
+      choices: ['Makeup Language', 'Markup Language', 'Programming Language', 'OOP Language'],
+      answer: 'Markup Language'
+    },
+    {
+      title: ' Who is making the Web standards?',
+      choices: ['Mozila', 'Google', 'World Wide Web', 'Microsoft'],
+      answer: 'World Wide Web'
+    },
+    {
+      title: 'What is the correct HTML element for inserting a line break?',
+      choices: ['<lb>', '<br>', '<h1>', '<break>'],
+      answer: '<br>'
+    }
+  ];
---
+++ question2.js
@@ -0,0 +1,28 @@
+// Questions as an array of Objects
+var questions = [
+    {
+      title: 'Which of the following is a component of CSS style rule?',
+      choices: ['All of the above.', 'Value', ' Property' , 'Selector'],
+      answer: 'All of the above.'
+    },
+    {
+      title: 'Which of the following property changes the color of left border?',
+      choices: [':border-bottom-color', ':border-top-color', 'parentheses', 'border-left-color'],
+      answer: 'border-left-color'
+    },
+    {
+      title: 'Which of the following value of cursor shows it as crosshair or plus sign?',
+      choices: ['move', 'pointer', 'crosshair', 'default'] ,
+      answer: 'crosshair'
+    },
+    {
+      title: 'Which of the following property of a anchor element signifies an element that currently has the user mouse pointer hovering over it?',
+      choices: ['activated', 'visited', 'hover', 'active'],
+      answer: 'hover'
+    },
+    {
+      title: 'Which of the following property is used to set the color of a text?',
+      choices: ['letter-spacing', 'direction', 'color', 'word-spacing'],
+      answer: 'color'
+    }
+  ];
